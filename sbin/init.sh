b23c88bdf2fc9500aed62a938339b58624a8fa70
Anurag Khandelwal <anuragk@berkeley.edu>
2016-01-29 15:30:44 -0800

    Added init script for hugepages

---
+++ sbin/init.sh
@@ -0,0 +1,121 @@
+#
+# Creates hugepage filesystem.
+#
+create_mnt_huge()
+{
+	echo "Creating /mnt/huge and mounting as hugetlbfs"
+	sudo mkdir -p /mnt/huge
+
+	grep -s '/mnt/huge' /proc/mounts > /dev/null
+	if [ $? -ne 0 ] ; then
+		sudo mount -t hugetlbfs nodev /mnt/huge
+	fi
+}
+
+#
+# Removes hugepage filesystem.
+#
+remove_mnt_huge()
+{
+	echo "Unmounting /mnt/huge and removing directory"
+	grep -s '/mnt/huge' /proc/mounts > /dev/null
+	if [ $? -eq 0 ] ; then
+		sudo umount /mnt/huge
+	fi
+
+	if [ -d /mnt/huge ] ; then
+		sudo rm -R /mnt/huge
+	fi
+}
+
+#
+# Removes all reserved hugepages.
+#
+clear_huge_pages()
+{
+	echo > .echo_tmp
+	for d in /sys/devices/system/node/node? ; do
+		echo "echo 0 > $d/hugepages/hugepages-2048kB/nr_hugepages" >> .echo_tmp
+	done
+	echo "Removing currently reserved hugepages"
+	sudo sh .echo_tmp
+	rm -f .echo_tmp
+
+	remove_mnt_huge
+}
+
+#
+# Creates hugepages.
+#
+set_non_numa_pages()
+{
+	clear_huge_pages
+
+	echo ""
+	echo "  Input the number of 2MB pages"
+	echo "  Example: to have 128MB of hugepages available, enter '64' to"
+	echo "  reserve 64 * 2MB pages"
+	echo -n "Number of pages: "
+	#read Pages
+	Pages=$1
+
+	echo "echo $Pages > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages" > .echo_tmp
+
+	echo "Reserving hugepages"
+	sudo sh .echo_tmp
+	rm -f .echo_tmp
+
+	create_mnt_huge
+}
+
+#
+# Creates hugepages on specific NUMA nodes.
+#
+set_numa_pages()
+{
+	clear_huge_pages
+
+	echo ""
+	echo "  Input the number of 2MB pages for each node"
+	echo "  Example: to have 128MB of hugepages available per node,"
+	echo "  enter '64' to reserve 64 * 2MB pages on each node"
+
+	echo > .echo_tmp
+	for d in /sys/devices/system/node/node? ; do
+		node=$(basename $d)
+		echo -n "Number of pages for $node: "
+		#read Pages
+		Pages=$1
+		shift
+		echo "echo $Pages > $d/hugepages/hugepages-2048kB/nr_hugepages" >> .echo_tmp
+	done
+	echo "Reserving hugepages"
+	sudo sh .echo_tmp
+	rm -f .echo_tmp
+
+	create_mnt_huge
+}
+
+#
+# Print hugepage information.
+#
+grep_meminfo()
+{
+	grep -i huge /proc/meminfo
+}
+
+#
+# List all hugepage file references
+#
+ls_mnt_huge()
+{
+	ls -lh /mnt/huge
+}
+
+set_numa_pages 512
+grep_meminfo
+
+# disable OOM kills
+sudo sysctl -w vm.overcommit_memory=1
+sudo sysctl -w kernel.shmmax=12884901888
+sudo sysctl -w kernel.shmall=12884901888
