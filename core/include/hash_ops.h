9aa814437b63fc5a9a004499f1ed8c2a63377dde
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-18 12:53:06 -0700

    Fixed HashOps

--- core/include/hash_ops.h
+++ core/include/hash_ops.h
@@ -10,13 +10,14 @@ class Hash {
   static const uint32_t K3 = 16777216;
 
   static uint32_t simple_hash(const char* buf) {
+    uint8_t* cast_buf = (uint8_t*) buf;
 #ifdef HASH4
-    return buf[0] * K3 + buf[1] * K2 + buf[2] * K1 + buf[3];
+    return cast_buf[0] * K3 + cast_buf[1] * K2 + cast_buf[2] * K1 + cast_buf[3];
 #else
 #ifdef HASH3
-    return buf[0] * K2 + buf[1] * K1 + buf[2];
+    return cast_buf[0] * K2 + cast_buf[1] * K1 + cast_buf[2];
 #else
-    return buf[0] * K1 + buf[1];
+    return cast_buf[0] * K1 + cast_buf[1];
 #endif
 #endif
   }
