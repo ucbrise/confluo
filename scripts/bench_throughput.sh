1691c3f370068c2814dfc2fd20aafa349a7518a5
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-19 22:28:47 +0000

    Benchmark script first kills all running servers on host

--- scripts/bench_throughput.sh
+++ scripts/bench_throughput.sh
@@ -3,6 +3,8 @@ hostname=${1:-localhost}
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
+ssh -o StrictHostKeyChecking=no $hostname "killall server"
+
 echo "Benchmarking server at $hostname"
 for i in 1 2 4 8 16 32 64; do
   for mix in "0.0-0.0-1.0-0.0" "0.5-0.5-0.0-0.0" "0.34-0.33-0.33-0.0"; do
