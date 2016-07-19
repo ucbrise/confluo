280cae22b21e7d520d0af63efe352d086225468e
Anurag Khandelwal <anuragk@berkeley.edu>
2016-07-19 22:27:48 +0000

    Fixed benchmark script

--- scripts/bench_throughput.sh
+++ scripts/bench_throughput.sh
@@ -8,7 +8,7 @@ for i in 1 2 4 8 16 32 64; do
   for mix in "0.0-0.0-1.0-0.0" "0.5-0.5-0.0-0.0" "0.34-0.33-0.33-0.0"; do
     ssh -o StrictHostKeyChecking=no $hostname "nohup /home/ec2-user/log-store/build/server/bin/server >/dev/null 2>/home/ec2-user/throughput-${mix}-${i}.log &"
     sleep 5
-    $sbin/../build/bench/bin/lsbench -b throughput-${mix} -n $i ~/tpch
+    $sbin/../build/bench/bin/lsbench -b throughput-${mix} -n $i -h $hostname ~/tpch
     ssh -o StrictHostKeyChecking=no $hostname "killall server"
   done
 done
