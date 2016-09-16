4e9a8d2f2ffd663362719e85e7210eb23eda0197
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-16 00:36:29 -0700

    Updated pcap file

--- scripts/parse_pcap.py
+++ scripts/parse_pcap.py
@@ -8,7 +8,12 @@ def main(f):
   pcap = dpkt.pcap.Reader(f)
   for ts, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
-    print "%d: %s" % (ts, eth.data.__class__.__name__)  
+    if eth.data.__class__.__name__ == 'IP' and (eth.data.data.__class__.__name__ == 'TCP' or eth.data.data.__class__.__name__ == 'UDP'):
+      attrs = (ts, eth.data.src, eth.data.dst, eth.data.data.sport, eth.data.data.dport)
+      print attrs
+    else:
+      attrs = (ts)
+      print attrs
 
 if __name__ == "__main__" :
   f = open(sys.argv[1])
