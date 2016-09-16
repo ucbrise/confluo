e6112ae8d022f0abebe50009cc289fe87957fe15
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-16 01:01:22 -0700

    Updated script

--- scripts/parse_pcap.py
+++ scripts/parse_pcap.py
@@ -6,15 +6,20 @@ import socket
 
 def main(f):
   pcap = dpkt.pcap.Reader(f)
+  wbytes = 0
   for ts, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
-    if eth.data.__class__.__name__ == 'IP' and (eth.data.data.__class__.__name__ == 'TCP' or eth.data.data.__class__.__name__ == 'UDP'):
-      attrs = (ts, eth.data.src, eth.data.dst, eth.data.data.sport, eth.data.data.dport)
-      print attrs
-    else:
-      attrs = (ts)
-      print attrs
+    attrs = (ts, wbytes, 0, 0, 0, 0)
+    if eth.type == dpkt.ethernet.ETH_TYPE_IP:
+      ip = eth.data
+      if ip.p == dpkt.ip.IP_PROTO_TCP:
+        tcp = ip.data
+        attrs = (ts, wbytes, ip.src, ip.dst, tcp.src, tcp.dst)
+    wbytes += len(buf)
+    print attrs
 
 if __name__ == "__main__" :
-  f = open(sys.argv[1])
+  inp = open(sys.argv[1])
+  outd = open(sys.argv[2] + ".data", 'wb')
+  outa = open(sys.argv[3] + ".attr", 'wb')
   main(f)
