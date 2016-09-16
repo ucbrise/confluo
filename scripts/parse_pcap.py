ef3a2796c830d3e369250669a4dedae4f7129c89
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-16 01:43:36 -0700

    Revert to dpkt

--- scripts/parse_pcap.py
+++ scripts/parse_pcap.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-try:
-    import scapy.all as scapy
-except ImportError:
-    import scapy
-    
+
+import dpkt
 import sys
+import socket
+
 def main(inp, outd, outa):
-  packets = scapy.rdpcap(inp)
+  pcap = dpkt.pcap.Reader(inp)
   wbytes = 0
-  for packet in packets:
-    attrs = (packet.time, wbytes, 0, 0, 0, 0)
-    if IP in packet and TCP in packet:
-      attrs = (packet.time, wbytes, pkt[IP].src, pkt[IP].dst, pkt[TCP].sport, pkt[TCP].dport)
+  for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    attrs = (ts, wbytes, 0, 0, 0, 0)
+    if eth.type == dpkt.ethernet.ETH_TYPE_IP:
+      ip = eth.data
+      if ip.p == dpkt.ip.IP_PROTO_TCP:
+        tcp = ip.data
+        attrs = (ts, wbytes, ip.src, ip.dst, tcp.sport, tcp.dport)
     wbytes += len(buf)
     print attrs
 
 if __name__ == "__main__" :
-  inp = sys.argv[1]
+  inp = open(sys.argv[1])
   outd = open(sys.argv[2] + ".data", 'wb')
   outa = open(sys.argv[2] + ".attr", 'wb')
   main(inp, outd, outa)
