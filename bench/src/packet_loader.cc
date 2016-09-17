0aab34cca55331c35de82aca41299796178ecc4f
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-17 16:40:20 -0700

    Fixed packet_loader benchmark

--- bench/src/packet_loader.cc
+++ bench/src/packet_loader.cc
@@ -79,7 +79,7 @@ void packet_loader::load_packets(const uint32_t num_threads,
                     timestamp_t start = get_timestamp();
                     while (idx < thread_ops && get_timestamp() - start < timebound) {
                       total_ops = insert_packet(idx++);
-                      local_ops += (total_ops > 0);
+                      local_ops++;
                       if (total_ops % kReportRecordInterval == 0) {
                         std::lock_guard<std::mutex> lock(report_mtx);
                         rfs << get_timestamp() << "\t" << total_ops << "\n";
