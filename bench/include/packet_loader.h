43b0fc8e7444a3663ba60d017028961446b0e34d
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-18 14:56:21 -0700

    Debugging

--- bench/include/packet_loader.h
+++ bench/include/packet_loader.h
@@ -42,7 +42,7 @@ class packet_loader {
 
   uint32_t parse_ip(std::string& ip) {
     char ip_raw[4];
-    sscanf(ip.c_str(), "%uhh.%uhh.%uhh.%uhh", &ip_raw[3], &ip_raw[2],
+    sscanf(ip.c_str(), "%u.%u.%u.%u", &ip_raw[3], &ip_raw[2],
            &ip_raw[1], &ip_raw[0]);
     uint32_t ret = ip_raw[0] | ip_raw[1] << 8 | ip_raw[2] << 16
         | ip_raw[3] << 24;
