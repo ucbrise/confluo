239331502ada643f03abbeea4f32b7a850dc6567
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-17 19:31:32 -0700

    Updated offset log

--- core/include/offsetlog.h
+++ core/include/offsetlog.h
@@ -10,46 +10,50 @@ namespace slog {
 
 class offsetlog {
  public:
+  struct offlen_entry {
+    offlen_entry() {
+      offlen = 0;
+      valid.store(false);
+    }
+
+    uint64_t offlen;
+    std::atomic<bool> valid;
+  };
+
   offsetlog() {
     current_id_.store(0L);
-    offlens_ = new __monolog_base <uint64_t, 32>;
-    valid_ = new std::atomic_bool[134217728];
-    for (uint64_t i = 0; i < 134217728; i++) {
-      valid_[i].store(false);
-    }
   }
 
   uint64_t start(uint64_t offset, uint16_t length) {
     uint64_t record_id = current_id_.fetch_add(1L);
     uint64_t offlen = ((uint64_t) length) << 48 | (offset & 0xFFFFFFFFFFFF);
-    offlens_->set(record_id, offlen);
+    offlens_[record_id].offlen = offlen;
     return record_id;
   }
 
   void end(uint64_t record_id) {
-    valid_[record_id].store(true);
+    offlens_[record_id].valid.store(true);
   }
 
   void lookup(uint64_t record_id, uint64_t& offset, uint16_t& length) {
-    uint64_t ol = offlens_->get(record_id);
+    uint64_t ol = offlens_[record_id].offlen;
     offset = ol & 0xFFFFFFFF;
     length = ol >> 48;
   }
 
   bool is_valid(uint64_t record_id) {
-    return record_id < current_id_.load() && valid_[record_id].load();
+    return record_id < current_id_.load() && offlens_[record_id].valid.load();
   }
 
   bool is_valid(uint64_t record_id, uint64_t max_rid) {
-    return record_id < max_rid && valid_[record_id].load() > 0;
+    return record_id < max_rid && offlens_[record_id].valid.load() > 0;
   }
 
   uint64_t num_ids() {
     return current_id_.load();
   }
 
-  __monolog_base <uint64_t, 32> *offlens_;
-  std::atomic_bool* valid_;
+  __monolog_base <offlen_entry, 32> offlens_;
   std::atomic<uint64_t> current_id_;
 };
 
