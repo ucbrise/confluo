dcff380a3c43fcb7047a39b476cd0514168b3d16
Anurag Khandelwal <anuragk@berkeley.edu>
2016-09-18 23:54:32 -0700

    Added rate limiter

--- bench/include/packet_loader.h
+++ bench/include/packet_loader.h
@@ -12,12 +12,60 @@
 #include <atomic>
 #include <thread>
 #include <mutex>
+#include <unistd.h>
 
 #include "logstore.h"
+
+#ifdef NO_LOG
+#define LOG(out, fmt, ...)
+#else
+#define LOG(out, fmt, ...) fprintf(out, fmt, ##__VA_ARGS__)
+#endif
 
 using namespace ::slog;
 using namespace ::std::chrono;
 
+class rate_limiter {
+ public:
+  rate_limiter(uint64_t pkts_per_sec, log_store::handle* handle) {
+    handle_ = handle;
+    dur_.tv_sec = 0;
+    dur_.tv_nsec = 0;
+    sleep_ns_ = 1e9 / pkts_per_sec;
+  }
+
+  uint64_t insert_packet(unsigned char* data, uint16_t len, tokens& tkns) {
+    auto t0 = high_resolution_clock::now();
+    uint64_t num_pkts = handle_->insert(data, len, tkns) + 1;
+    auto t1 = high_resolution_clock::now();
+    dur_.tv_nsec = sleep_ns_ - duration_cast<nanoseconds>(t1 - t0).count();
+    if (dur_.tv_nsec > 0) {
+      nanosleep(&dur_, NULL);
+    }
+    return num_pkts;
+  }
+
+ private:
+  struct timespec dur_;
+  int64_t sleep_ns_;
+  log_store::handle* handle_;
+};
+
+class rate_limiter_inf {
+ public:
+  rate_limiter_inf(uint64_t pkts_per_sec, log_store::handle* handle) {
+    handle_ = handle;
+  }
+
+  uint64_t insert_packet(unsigned char* data, uint16_t len, tokens& tkns) {
+    return handle_->insert(data, len, tkns) + 1;
+  }
+
+ private:
+  log_store::handle* handle_;
+};
+
+template<class rlimiter = rate_limiter_inf>
 class packet_loader {
  public:
   typedef unsigned long long int timestamp_t;
@@ -25,13 +73,16 @@ class packet_loader {
 
   uint64_t insert_packet(log_store::handle* handle, uint64_t idx) {
     tokens tkns;
+    init_tokens(tkns, idx);
+    return handle->insert(datas_[idx], datalens_[idx], tkns) + 1;
+  }
+
+  void init_tokens(tokens& tkns, uint64_t idx) {
     tkns.time = (unsigned char*) (&timestamps_[idx]);
     tkns.src_ip = (unsigned char*) (&srcips_[idx]);
     tkns.dst_ip = (unsigned char*) (&dstips_[idx]);
     tkns.src_prt = (unsigned char*) (&sports_[idx]);
     tkns.dst_prt = (unsigned char*) (&dports_[idx]);
-
-    return handle->insert(datas_[idx], datalens_[idx], tkns) + 1;
   }
 
   uint32_t parse_ip(std::string& ip) {
@@ -51,12 +102,106 @@ class packet_loader {
   }
 
   packet_loader(std::string& data_path, std::string& attr_path,
-                std::string& hostname);
+                std::string& hostname) {
+    char resolved_path[100];
+    realpath(data_path.c_str(), resolved_path);
+    data_path_ = std::string(resolved_path);
+    realpath(attr_path.c_str(), resolved_path);
+    attr_path_ = std::string(resolved_path);
+    hostname_ = hostname;
 
-  void load_data();
+    logstore_ = new log_store();
+
+    LOG(stderr, "Loading data...\n");
+    load_data();
+
+    LOG(stderr, "Initialization complete.\n");
+  }
+
+  void load_data() {
+    std::ifstream ind(data_path_);
+    std::ifstream ina(attr_path_);
+    std::string attr_line;
+    LOG(stderr, "Reading from path data=%s, attr=%s\n", data_path_.c_str(),
+        attr_path_.c_str());
+
+    while (std::getline(ina, attr_line)) {
+      std::stringstream attr_stream(attr_line);
+      uint32_t ts;
+      std::string srcip, dstip;
+      uint16_t sport, dport;
+      uint16_t len;
+      attr_stream >> ts >> len >> srcip >> dstip >> sport >> dport;
+      unsigned char* data = new unsigned char[len];
+      ind.read((char*) data, len);
+      timestamps_.push_back(parse_time(ts));
+      srcips_.push_back(parse_ip(srcip));
+      dstips_.push_back(parse_ip(srcip));
+      sports_.push_back(parse_port(sport));
+      dports_.push_back(parse_port(dport));
+      datas_.push_back(data);
+      datalens_.push_back(len);
+    }
+
+    LOG(stderr, "Loaded %zu packets.\n", datas_.size());
+  }
 
   // Throughput benchmarks
-  void load_packets(const uint32_t num_clients, const uint64_t timebound);
+  void load_packets(const uint32_t num_threads, const uint64_t timebound,
+                    const uint64_t rate_limit) {
+
+    std::vector<std::thread> threads;
+    uint64_t thread_ops = timestamps_.size() / num_threads;
+    uint64_t local_rate_limit = rate_limit / num_threads;
+    for (uint32_t i = 0; i < num_threads; i++) {
+      threads.push_back(
+          std::move(
+              std::thread(
+                  [i, timebound, local_rate_limit, thread_ops, this] {
+                    uint64_t idx = thread_ops * i;
+                    tokens tkns;
+                    log_store::handle* handle = logstore_->get_handle();
+                    rlimiter* limiter = new rlimiter(local_rate_limit, handle);
+                    double throughput = 0;
+                    std::ofstream rfs;
+                    rfs.open("record_progress_" + std::to_string(i));
+                    LOG(stderr, "Starting benchmark.\n");
+
+                    try {
+                      int64_t local_ops = 0;
+                      int64_t total_ops = 0;
+
+                      timestamp_t start = get_timestamp();
+                      while (local_ops < thread_ops && get_timestamp() - start < timebound) {
+                        init_tokens(tkns, idx);
+                        total_ops = limiter->insert_packet(datas_[idx], datalens_[idx], tkns);
+                        idx++;
+                        local_ops++;
+                        if (total_ops % kReportRecordInterval == 0) {
+                          rfs << get_timestamp() << "\t" << total_ops << "\n";
+                        }
+                      }
+                      timestamp_t end = get_timestamp();
+                      double totsecs = (double) (end - start) / (1000.0 * 1000.0);
+                      throughput = ((double) local_ops / totsecs);
+                      LOG(stderr, "Thread #%u finished in %lf s. Throughput: %lf.\n", i, totsecs, throughput);
+                    } catch (std::exception &e) {
+                      LOG(stderr, "Throughput thread ended prematurely.\n");
+                    }
+
+                    std::ofstream ofs("write_throughput_" + std::to_string(i));
+                    ofs << throughput << "\n";
+                    ofs.close();
+                    delete limiter;
+                    delete handle;
+                  })));
+    }
+
+    for (auto& th : threads) {
+      th.join();
+    }
+
+  }
 
  private:
   static timestamp_t get_timestamp() {
--- bench/src/packet_loader.cc
+++ bench/src/packet_loader.cc
@@ -1,107 +1,5 @@
 #include "packet_loader.h"
 
-#include <unistd.h>
-
-#ifdef NO_LOG
-#define LOG(out, fmt, ...)
-#else
-#define LOG(out, fmt, ...) fprintf(out, fmt, ##__VA_ARGS__)
-#endif
-
-packet_loader::packet_loader(std::string& data_path, std::string& attr_path,
-                             std::string& hostname) {
-  char resolved_path[100];
-  realpath(data_path.c_str(), resolved_path);
-  data_path_ = std::string(resolved_path);
-  realpath(attr_path.c_str(), resolved_path);
-  attr_path_ = std::string(resolved_path);
-  hostname_ = hostname;
-
-  logstore_ = new log_store();
-
-  LOG(stderr, "Loading data...\n");
-  load_data();
-
-  LOG(stderr, "Initialization complete.\n");
-}
-
-void packet_loader::load_data() {
-  std::ifstream ind(data_path_);
-  std::ifstream ina(attr_path_);
-  std::string attr_line;
-  LOG(stderr, "Reading from path data=%s, attr=%s\n", data_path_.c_str(),
-      attr_path_.c_str());
-
-  while (std::getline(ina, attr_line)) {
-    std::stringstream attr_stream(attr_line);
-    uint32_t ts;
-    std::string srcip, dstip;
-    uint16_t sport, dport;
-    uint16_t len;
-    attr_stream >> ts >> len >> srcip >> dstip >> sport >> dport;
-    unsigned char* data = new unsigned char[len];
-    ind.read((char*) data, len);
-    timestamps_.push_back(parse_time(ts));
-    srcips_.push_back(parse_ip(srcip));
-    dstips_.push_back(parse_ip(srcip));
-    sports_.push_back(parse_port(sport));
-    dports_.push_back(parse_port(dport));
-    datas_.push_back(data);
-    datalens_.push_back(len);
-  }
-
-  LOG(stderr, "Loaded %zu packets.\n", datas_.size());
-}
-
-void packet_loader::load_packets(const uint32_t num_threads,
-                                 const uint64_t timebound) {
-
-  std::vector<std::thread> threads;
-  uint64_t thread_ops = timestamps_.size() / num_threads;
-  for (uint32_t i = 0; i < num_threads; i++) {
-    threads.push_back(
-        std::move(
-            std::thread(
-                [i, timebound, thread_ops, this] {
-                  uint64_t idx = thread_ops * i;
-                  log_store::handle* handle = logstore_->get_handle();
-                  double throughput = 0;
-                  std::ofstream rfs;
-                  rfs.open("record_progress_" + std::to_string(i));
-                  LOG(stderr, "Starting benchmark.\n");
-
-                  try {
-                    int64_t local_ops = 0;
-                    int64_t total_ops = 0;
-
-                    timestamp_t start = get_timestamp();
-                    while (local_ops < thread_ops && get_timestamp() - start < timebound) {
-                      total_ops = insert_packet(handle, idx++);
-                      local_ops++;
-                      if (total_ops % kReportRecordInterval == 0) {
-                        rfs << get_timestamp() << "\t" << total_ops << "\n";
-                      }
-                    }
-                    timestamp_t end = get_timestamp();
-                    double totsecs = (double) (end - start) / (1000.0 * 1000.0);
-                    throughput = ((double) local_ops / totsecs);
-                    LOG(stderr, "Thread #%u finished in %lf s. Throughput: %lf.\n", totsecs, throughput);
-                  } catch (std::exception &e) {
-                    LOG(stderr, "Throughput thread ended prematurely.\n");
-                  }
-
-                  std::ofstream ofs("write_throughput_" + std::to_string(i));
-                  ofs << throughput << "\n";
-                  ofs.close();
-                })));
-  }
-
-  for (auto& th : threads) {
-    th.join();
-  }
-
-}
-
 void PrintUsage(char *exec) {
   LOG(stderr, "Usage: %s -h [hostname] -n [numthreads] [data] [attrs]\n", exec);
 }
@@ -123,7 +21,7 @@ std::vector<std::string> Split(const std::string &s, char delim) {
 }
 
 int main(int argc, char** argv) {
-  if (argc < 3 || argc > 9) {
+  if (argc < 3 || argc > 11) {
     PrintUsage(argv[0]);
     return -1;
   }
@@ -133,7 +31,8 @@ int main(int argc, char** argv) {
   int num_clients = 1;
   uint8_t num_attributes = 1;
   uint64_t timebound = UINT64_MAX;
-  while ((c = getopt(argc, argv, "t:n:h:")) != -1) {
+  uint64_t rate_limit = 0;
+  while ((c = getopt(argc, argv, "t:n:h:r:")) != -1) {
     switch (c) {
       case 't':
         timebound = atol(optarg) * 10e6;
@@ -143,6 +42,9 @@ int main(int argc, char** argv) {
         break;
       case 'h':
         hostname = std::string(optarg);
+        break;
+      case 'r':
+        rate_limit = atoll(optarg);
         break;
       default:
         LOG(stderr, "Could not parse command line arguments.\n");
@@ -157,8 +59,13 @@ int main(int argc, char** argv) {
   std::string data_path = std::string(argv[optind]);
   std::string attr_path = std::string(argv[optind + 1]);
 
-  packet_loader loader(data_path, attr_path, hostname);
-  loader.load_packets(num_clients, timebound);
+  if (rate_limit == 0) {
+    packet_loader<> loader(data_path, attr_path, hostname);
+    loader.load_packets(num_clients, timebound, 0);
+  } else {
+    packet_loader<rate_limiter> loader(data_path, attr_path, hostname);
+    loader.load_packets(num_clients, timebound, rate_limit);
+  }
 
   return 0;
 }
