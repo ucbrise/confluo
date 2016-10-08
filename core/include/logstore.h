29f02a40d51f4ebc1c299f0e60b9cf8adb4c0b81
Anurag Khandelwal <anuragk@berkeley.edu>
2016-10-08 10:22:35 -0700

    Fixed refactoring errors

--- core/include/logstore.h
+++ core/include/logstore.h
@@ -23,7 +23,6 @@
 #include "indexlog.h"
 #include "streamlog.h"
 #include "offsetlog.h"
-#include "handle.h"
 #include "filterops.h"
 #include "utils.h"
 #include "exceptions.h"
@@ -47,7 +46,57 @@ struct logstore_storage {
 
 class log_store {
  public:
-  friend class handle;
+  class handle {
+   public:
+    handle(log_store& handle, uint64_t request_batch_size = 256,
+           uint64_t data_block_size = 256 * 64)
+        : handle_(handle) {
+      id_block_size_ = request_batch_size;
+      remaining_ids_ = 0;
+      cur_id_ = 0;
+
+      data_block_size_ = data_block_size;
+      remaining_bytes_ = 0;
+      cur_offset_ = 0;
+    }
+
+    uint64_t insert(const unsigned char* record, uint16_t record_len,
+                    token_list& tkns) {
+      if (remaining_ids_ == 0) {
+        cur_id_ = handle_.olog_->request_id_block(id_block_size_);
+        remaining_ids_ = id_block_size_;
+      }
+
+      if (remaining_bytes_ < record_len) {
+        cur_offset_ = handle_.request_bytes(data_block_size_);
+        remaining_bytes_ = data_block_size_;
+      }
+
+      handle_.append_record(record, record_len, cur_offset_);
+      handle_.update_indexes(cur_id_, tkns);
+      handle_.olog_->set(cur_id_, cur_offset_, record_len);
+      handle_.olog_->end(cur_id_);
+      remaining_ids_--;
+      cur_offset_ += record_len;
+      return ++cur_id_;
+    }
+
+    const bool get(unsigned char* record, const int64_t record_id) {
+      return handle_.get(record, record_id);
+    }
+
+   private:
+    uint64_t data_block_size_;
+    uint64_t id_block_size_;
+
+    uint64_t cur_id_;
+    uint64_t remaining_ids_;
+
+    uint64_t cur_offset_;
+    uint64_t remaining_bytes_;
+
+    log_store& handle_;
+  };
 
   /**
    * Constructor to initialize the log-store.
@@ -126,7 +175,7 @@ class log_store {
    * @return The unique record id generated for the record.
    */
   uint64_t insert(const unsigned char* record, uint16_t record_len,
-                  const token_list& tokens) {
+                  token_list& tokens) {
     /* Atomically request bytes at the end of data-log */
     uint64_t offset = request_bytes(record_len);
 
@@ -262,8 +311,7 @@ class log_store {
    * @param results The results of the filter query.
    * @param query The filter query.
    */
-  void filter(std::unordered_set<uint64_t>& results,
-              const filter_query& query) {
+  void filter(std::unordered_set<uint64_t>& results, filter_query& query) {
     uint64_t max_rid = olog_->num_ids();
     for (filter_conjunction& conjunction : query) {
       std::unordered_set<uint64_t> conjunction_results;
@@ -350,7 +398,7 @@ class log_store {
    * @param record_id The id of the record whose index entries are being added.
    * @param tokens The tokens associated with the record.
    */
-  void update_indexes(uint64_t record_id, const token_list& tokens) {
+  void update_indexes(uint64_t record_id, token_list& tokens) {
     for (token_t& token : tokens) {
       /* Identify which index token belongs to */
       uint32_t idx = token.index_id / OFFSETMIN;
@@ -545,6 +593,7 @@ class log_store {
   /* Stream logs */
   monolog_linearizable<streamlog*> *streams_;
 };
+
 }
 
 #endif /* SLOG_LOGSTORE_H_ */
