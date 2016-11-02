36a1316460bfc79146490d225394e424d26a047f
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-02 01:38:59 -0700

    Added tiered index to logstore

--- core/include/tokens.h
+++ core/include/tokens.h
@@ -6,11 +6,30 @@
 
 namespace slog {
 
-typedef struct {
-  uint32_t index_id;
-  unsigned char* data;
-  size_t len;
-} token_t;
+struct token_t {
+ public:
+  token_t(uint32_t index_id, unsigned char* data) {
+    index_id_ = index_id;
+    data_ = *(uint64_t*) data;
+  }
+
+  token_t(uint32_t index_id, uint64_t data) {
+    index_id_ = index_id;
+    data_ = data;
+  }
+
+  uint32_t index_id() {
+    return index_id_;
+  }
+
+  uint64_t data() {
+    return data_;
+  }
+
+ private:
+  uint32_t index_id_;
+  uint64_t data_;
+};
 
 typedef std::vector<token_t> token_list;
 
