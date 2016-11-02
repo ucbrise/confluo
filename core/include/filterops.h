4851dbe3079b26da24e3f92433640659d323bd33
Anurag Khandelwal <anuragk@berkeley.edu>
2016-11-02 01:38:59 -0700

    Added tiered index to logstore

--- core/include/filterops.h
+++ core/include/filterops.h
@@ -4,10 +4,45 @@
 namespace slog {
 
 struct basic_filter {
-  // bool negate; // TODO Add negation
-  uint32_t index_id;
-  unsigned char* token_prefix;
-  uint32_t token_prefix_len;
+ public:
+  basic_filter(uint32_t index_id, unsigned char* token_beg,
+               unsigned char* token_end) {
+    index_id_ = index_id;
+    token_beg_ = *(uint64_t*) token_beg;
+    token_end_ = *(uint64_t*) token_end;
+  }
+
+  basic_filter(uint32_t index_id, uint64_t token_beg, uint64_t token_end) {
+    index_id_ = index_id;
+    token_beg_ = token_beg;
+    token_end_ = token_end;
+  }
+
+  basic_filter(uint32_t index_id, unsigned char* token)
+      : basic_filter(index_id, token, token) {
+  }
+
+  basic_filter(uint32_t index_id, uint64_t token)
+      : basic_filter(index_id, token, token) {
+  }
+
+  uint32_t index_id() {
+    return index_id_;
+  }
+
+  uint64_t token_beg() {
+    return token_beg_;
+  }
+
+  uint64_t token_end() {
+    return token_end_;
+  }
+
+ private:
+  uint32_t index_id_;
+  uint64_t token_beg_;
+  uint64_t token_end_;
+  /* TODO: Add negation */
 };
 
 typedef std::vector<basic_filter> filter_conjunction;
