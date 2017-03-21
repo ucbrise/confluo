8ed8e936800d66fdc03abb3df0b79a40e65c4894
Anurag Khandelwal <anuragk@berkeley.edu>
2017-03-21 16:56:59 -0700

    Removed assertions

--- app/logstore/libls/perf/perf_main.cc
+++ app/logstore/libls/perf/perf_main.cc
@@ -7,12 +7,14 @@ extern std::string res_path_stream;
 extern std::string res_path_logstore;
 
 extern bool filter_fn1(uint64_t& record_id, const unsigned char* record,
-                const uint16_t record_len, const monolog::token_list& list) {
+                       const uint16_t record_len,
+                       const monolog::token_list& list) {
   return record_id % 10 == 0;
 }
 
 extern bool filter_fn2(uint64_t& record_id, const unsigned char* record,
-                const uint16_t record_len, const monolog::token_list& list) {
+                       const uint16_t record_len,
+                       const monolog::token_list& list) {
   bool filter = record_id % 10 == 0;
   record_id /= 10;
   return filter;
@@ -20,8 +22,9 @@ extern bool filter_fn2(uint64_t& record_id, const unsigned char* record,
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  assert(argc == 2);
-  std::string res_path = std::string(argv[1]);
+  std::string res_path = ".";
+  if (argc >= 2)
+    res_path = std::string(argv[1]);
   res_path_index = res_path + "/index_latency.txt";
   res_path_index2 = res_path + "/index";
   res_path_stream = res_path + "/stream";
--- libmonolog/perf/perf_main.cc
+++ libmonolog/perf/perf_main.cc
@@ -4,8 +4,9 @@ extern std::string res_path_monolog;
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  assert(argc == 2);
-  std::string res_path = std::string(argv[1]);
+  std::string res_path = ".";
+  if (argc >= 2)
+    res_path = std::string(argv[1]);
   res_path_monolog = res_path + "/monolog";
   return RUN_ALL_TESTS();
 }
