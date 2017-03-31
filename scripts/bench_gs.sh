9a3a5bea1276f264b0e299bab6b347e615d91a7b
EC2 Default User <ec2-user@ip-172-31-79-214.ec2.internal>
2017-03-31 02:05:15 +0000

    Updated bench script

--- scripts/bench_gs.sh
+++ scripts/bench_gs.sh
@@ -3,7 +3,6 @@
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
-res_dir=${1:-"results"}
 ops=(
 add_node
 update_node
@@ -16,9 +15,12 @@ get_link
 get_link_list
 count_links
 )
-for op in ${ops[@]}; do
-  for num_threads in 1 2 4 8 16 32; do
-    $sbin/../build/app/graphstore/libgs/bin/gsperf --num-threads=$num_threads\
-      --bench-type=$op --output-dir=$res_dir
+
+for tail_scheme in "read-stalled" "write-stalled"; do
+  for op in ${ops[@]}; do
+    for num_threads in 1 2 4 8 16 32 64 128; do
+      $sbin/../build/app/graphstore/libgs/bin/gsperf --num-threads=$num_threads\
+        --bench-type=$op --tail-scheme=$tail_scheme --output-dir=$tail_scheme
+    done
   done
 done
