e8fc28c5bbe3bd5f41b63612094d133608831b8d
Anurag Khandelwal <anuragk@berkeley.edu>
2017-04-01 11:23:59 -0700

    Added mmap readonly mode

--- libutils/utils/mmap_utils.h
+++ libutils/utils/mmap_utils.h
@@ -8,15 +8,35 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 
 #include "assertions.h"
 
+#ifndef MAP_POPULATE
+#define MAP_POPULATE 0
+#endif
+
 namespace utils {
 
 class mmap_utils {
  public:
+  static void* mmap_r(const std::string path) {
+    int fd = open(path.c_str(), O_RDONLY, (S_IRWXU | S_IRWXG | S_IRWXO));
+    assert_throw(fd >= 0, "Could not open file " << path << " for mmap");
+
+    struct stat st;
+    stat(path.c_str(), &st);
+    size_t size = st.st_size;
+
+    void* data = mmap(NULL, size, PROT_READ, MAP_SHARED | MAP_POPULATE, fd, 0);
+    assert_throw(data != MAP_FAILED, "Could not mmap file " << path);
+
+    return data;
+  }
+
   static void* mmap_rw(const std::string path, size_t size) {
     int fd = open(path.c_str(), (O_CREAT | O_TRUNC | O_RDWR),
                   (S_IRWXU | S_IRWXG | S_IRWXO));
