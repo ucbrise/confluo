fc9bd4aef23c62d290e27839820270bedac3bc7f
Anurag Khandelwal <anuragk@berkeley.edu>
2017-04-10 23:08:35 -0700

    Print messages

--- libds/src/server/server.cc
+++ libds/src/server/server.cc
@@ -53,6 +53,8 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  LOG_INFO << parser.parsed_values();
+
   if (!append_only) {
     if (storage_scheme == "in-memory") {
       if (concurrency_control == "write-stalled") {
