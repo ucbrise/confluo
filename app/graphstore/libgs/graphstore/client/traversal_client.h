f8f653143c07e1251ae1b4a147bb43135c88e74c
Anurag Khandelwal <anuragk@berkeley.edu>
2017-04-16 01:38:20 -0700

    Debug

--- app/graphstore/libgs/graphstore/client/traversal_client.h
+++ app/graphstore/libgs/graphstore/client/traversal_client.h
@@ -44,13 +44,18 @@ class traversal_client {
   void traverse(std::vector<TLink>& _return, const int64_t id,
       const int64_t link_type, const int64_t depth, const int64_t breadth) {
 
+    uint64_t t1 = utils::time_utils::cur_ns();
     const snapshot& s = coord_->force_snapshot();
     std::vector<int64_t> snapshots;
     for (const auto& t : s.tails) {
       snapshots.push_back(t);
     }
+    uint64_t t2 = utils::time_utils::cur_ns();
     size_t client_id = id % hosts_.size();
+    uint64_t t3 = utils::time_utils::cur_ns();
     clients_.at(client_id).traverse(_return, id, link_type, depth, breadth, snapshots);
+    uint64_t t4 = utils::time_utils::cur_ns();
+    LOG_INFO << (t2-t1) << "\t" << (t4 - t3) << "\t" << _return.size();
   }
 
 private:
