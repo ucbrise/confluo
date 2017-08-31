e256f88aeebec539c3e4e0935b574533ec23be71
Ujval Misra <misraujval@gmail.com>
2017-08-31 16:36:34 -0700

    Python client
    * Python client functionality
    
    * Python client tests

---
+++ pyclient/dialog/storage.py
@@ -0,0 +1,7 @@
+from dialog.ttypes import *
+
+# Alias for rpc_storage_mode
+class storage_id:
+    IN_MEMORY = rpc_storage_mode.RPC_IN_MEMORY
+    DURABLE_RELAXED = rpc_storage_mode.RPC_DURABLE_RELAXED
+    DURABLE = rpc_storage_mode.RPC_DURABLE
