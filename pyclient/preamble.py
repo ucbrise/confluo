c9a52aaec92318d62c5b499c5da6dd900db49be6
Anurag Khandelwal <anuragk@berkeley.edu>
2017-09-06 14:30:49 -0700

    Updates

--- pyclient/preamble.py
+++ pyclient/preamble.py
@@ -3,33 +3,25 @@
 import os
 import sys
 from thrift import Thrift
-
-lib_path = os.path.dirname(os.path.realpath(sys.argv[0])) + "/lib"
-print "Importing DiaLog python client libraries from %s..." % lib_path
-
-sys.path.append(lib_path)
-
 from dialog import dialog_client
 
 dialog = None
 
 def dialogConnect(host = 'localhost', port = 9090):
   global dialog
-
-  # Create a client
   dialog = dialog_client.dialog_client(host, port)  
-  print "DiaLog Client is now available as dialog."
   
+
 try:
   dialogConnect()
-  
+  print "DiaLog Client is now available as dialog."
 except Thrift.TException, tx:
   print '%s' % (tx.message)
   print 'Check your server status and retry connecting with dialogConnect(host, port)'
 
 # Add auto-completion and a stored history file of commands to your Python
 # interactive interpreter. Requires Python 2.0+, readline. Autocomplete is
-# bound to the Esc key by default (you can change it - see readline docs).
+# bound to the Esc key by default.
 #
 # Store the file in ~/.pystartup, and set an environment variable to point
 # to it:  "export PYTHONSTARTUP=~/.pystartup" in bash.
--- pyclient/setup.py
+++ pyclient/setup.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools import Command
+import sys
+import os
+
+class DialogShell(Command):
+    """ Run DiaLog shell
+    """
+    
+    description = "Run interactive DiaLog shell"
+    
+    user_options = [('hostname', 'h', 'Server hostname'), 
+                    ('port', 'p', 'Server port')]
+    
+    def initialize_options(self):
+        self.hostname = "localhost"
+        self.port = "9090"
+        
+    def finalize_options(self):
+        if self.hostname is None:
+            raise Exception("Parameter --port is missing")
+        if self.port is None:
+            raise Exception("Parameter --port is missing")
+    
+    def run(self):
+        python_ex = sys.executable
+        os.execv(python_ex, [ python_ex, "-i", "preamble.py"])
 
 setup(name='dialog',
       version='0.1.0',
@@ -10,7 +36,8 @@ setup(name='dialog',
       url='https://www.github.com/ucbrise/dialog',
       package_dir={'dialog': 'dialog'},
       packages=['dialog'],
-      setup_requires=['pytest-runner', ],
+      setup_requires=['pytest-runner', 'thrift>=0.10.0'],
       tests_require=['pytest-cov', 'pytest', 'thrift>=0.10.0', ],
       install_requires=['thrift>=0.10.0', ],
+      cmdclass={'shell' : DialogShell,}
       )
