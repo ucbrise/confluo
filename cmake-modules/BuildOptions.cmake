28fe2512c10487a7d10edc9b49d27c306b9dabf0
Anurag Khandelwal <anuragk@berkeley.edu>
2017-09-08 15:57:56 -0700

    Added build options

---
+++ cmake-modules/BuildOptions.cmake
@@ -0,0 +1,15 @@
+include(CMakeDependentOption)
+
+# Components to build
+option(BUILD_TESTS "Build with unittests" ON)
+option(BUILD_RPC "Build RPC framework" ON)
+CMAKE_DEPENDENT_OPTION(WITH_PY_CLIENT "Build python client" ON "BUILD_RPC" OFF)
+CMAKE_DEPENDENT_OPTION(WITH_JAVA_CLIENT "Build java client" ON "BUILD_RPC" OFF)
+
+message(STATUS "----------------------------------------------------------")
+message(STATUS "Build configuration Summary")
+message(STATUS "  Build RPC Framework:                    ${BUILD_RPC}")
+message(STATUS "    Build python client libraries:        ${WITH_PY_CLIENT}")
+message(STATUS "    Build java client libraries:          ${WITH_JAVA_CLIENT}")
+message(STATUS "  Build with unit tests:                  ${BUILD_TESTS}")
+message(STATUS "----------------------------------------------------------")
