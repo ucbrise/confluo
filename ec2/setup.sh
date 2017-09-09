dae32485ad9825d86259bf62ebd6dfb1e6b93355
Anurag Khandelwal <anuragk@berkeley.edu>
2017-09-08 18:04:02 -0700

    Manually install cmake

--- ec2/setup.sh
+++ ec2/setup.sh
@@ -5,7 +5,14 @@ curdir="`cd "$curdir"; pwd`"
 
 sudo yum -y update
 sudo yum -y groupinstall "Development Tools"
-sudo yum -y install gcc-c++ cmake boost-devel java-1.7.0-openjdk* ant
+sudo yum -y install gcc-c++ boost-devel java-1.7.0-openjdk* ant
+
+wget https://cmake.org/files/v3.9/cmake-3.9.2.tar.gz
+tar xvzf cmake-3.9.2.tar.gz
+cd cmake-3.9.2
+./bootstrap
+make
+make install
 
 cd $curdir/..
 mkdir build
