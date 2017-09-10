8fe09f780d238039136699db6e005c077715d35c
Anurag Khandelwal <anuragk@berkeley.edu>
2017-09-09 21:20:10 -0700

    Added chain setup scripts

---
+++ sbin/setup-chain.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+reverse() {
+  sed '1!G;h;$!d' $1
+}
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+. "$sbin/dialog-config.sh"
+. "$DIALOG_PREFIX/sbin/load-dialog-env.sh"
+
+SERVERLIST=`reverse ${DIALOG_CONF_DIR}/hosts`
+
+# By default disable strict host key checking
+if [ "$DIALOG_SSH_OPTS" = "" ]; then
+  DIALOG_SSH_OPTS="-o StrictHostKeyChecking=no"
+fi
+
+if [ "$DIALOG_PATH" == "" ]; then
+  DIALOG_PATH="$HOME/dialog"
+fi
+
+if [ "$DIALOG_PORT" == "" ]; then
+  DIALOG_PORT="9090"
+fi
+
+HOSTS=`echo "$SERVERLIST"|sed  "s/#.*$//;/^$/d"`
+SAVEIFS=$IFS
+IFS=$'\n'
+HOSTS=($HOSTS)
+IFS=$SAVEIFS
+
+TAIL=${HOSTS[0]}
+echo "Setting up tail=$TAIL"
+ssh $DIALOG_SSH_OPTS $TAIL $DIALOG_PATH/sbin/start-dialog.sh \
+  --endpoint "0.0.0.0:$DIALOG_PORT" \
+  2>&1 | sed "s/^/$host: /"
+for (( i=1; i<${#HOSTS[@]}; i++ )); do
+  HOST=${HOSTS[$i]}
+  SUCCESSOR=${HOSTS[$i-1]}
+  echo "Setting up host=$HOST with successor=$SUCCESSOR, tail=$TAIL"
+  ssh $DIALOG_SSH_OPTS $HOST $DIALOG_PATH/sbin/start-dialog.sh \
+    --endpoint "0.0.0.0:$DIALOG_PORT" \
+    --successor "$SUCCESSOR:$DIALOG_PORT" \
+    --tail "$TAIL:$DIALOG_PORT" \
+    2>&1 | sed "s/^/$host: /"
+done
--- sbin/start-dialog.sh
+++ sbin/start-dialog.sh
@@ -9,12 +9,8 @@ sbin="`cd "$sbin"; pwd`"
 LOG_PATH="$sbin/../log"
 mkdir -p $LOG_PATH
 
-if [ "$BIND_ADDRESS" = "" ]; then
-  BIND_ADDRESS="0.0.0.0"
-fi
-
-if [ "$BIND_PORT" = "" ]; then
-  BIND_PORT="9090"
+if [ "$HOST_ENDPOINT" = "" ]; then
+  HOST_ENDPOINT="0.0.0.0:9090"
 fi
 
 if [ "$DATA_PATH" = "" ]; then
@@ -23,5 +19,5 @@ if [ "$DATA_PATH" = "" ]; then
 fi
 
 mkdir -p $LOG_PATH
-$sbin/../build/bin/dialogd --address=$BIND_ADDRESS --port $BIND_PORT \
-  --data-path $DATA_PATH 2>$LOG_PATH/dialog.stderr 1>$LOG_PATH/dialog.stdout &
+$sbin/../build/bin/dialogd --endpoint=$HOST_ENDPOINT --data-path $DATA_PATH $@\
+   2>$LOG_PATH/dialog.stderr 1>$LOG_PATH/dialog.stdout &
