b51bf3b3580afcf04b1a79e98ee91335a24128ad
Anurag Khandelwal <anuragk@berkeley.edu>
2017-09-09 21:48:07 -0700

    Add destroy chain script

---
+++ sbin/destroy-chain.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+reverse() {
+  sed '1!G;h;$!d' $1
+}
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+. "$sbin/dialog-config.sh"
+. "$DIALOG_PREFIX/sbin/load-dialog-env.sh"
+
+SERVERLIST=`reverse ${DIALOG_CONF_DIR}/hosts`
+
+HOSTS=`echo "$SERVERLIST"|sed  "s/#.*$//;/^$/d"`
+SAVEIFS=$IFS
+IFS=$'\n'
+HOSTS=($HOSTS)
+IFS=$SAVEIFS
+
+for (( i=0; i<${#HOSTS[@]}; i++ )); do
+  echo "Taking down ${HOSTS[$i]}"
+  ssh $DIALOG_SSH_OPTS $HOST $DIALOG_PATH/sbin/stop-dialog.sh \
+    2>&1 | sed "s/^/$host: /"
+done
