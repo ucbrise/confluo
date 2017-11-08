686a7cb05320060855862110d0ceb60a361f37ee
Ujval Misra <misraujval@gmail.com>
2017-11-07 20:12:20 -0800

    Configuration parameter for archival.

--- libconfluo/confluo/container/data_log.h
+++ libconfluo/confluo/container/data_log.h
@@ -11,16 +11,16 @@ namespace confluo {
 class data_log_constants {
  public:
   /** Maximum number of blocks */
-  static constexpr size_t MAX_BLOCKS = 65536;
+  static const size_t MAX_BLOCKS = 65536;
   /** The size of each block */
-  static constexpr size_t BLOCK_SIZE = 67108864;
+  static const size_t BLOCK_SIZE = 67108864;
   /** The size of the buffer */
-  static constexpr size_t BUFFER_SIZE = 1048576;
+  static const size_t BUFFER_SIZE = 1048576;
 };
 
-constexpr size_t data_log_constants::MAX_BLOCKS;
-constexpr size_t data_log_constants::BLOCK_SIZE;
-constexpr size_t data_log_constants::BUFFER_SIZE;
+const size_t data_log_constants::MAX_BLOCKS;
+const size_t data_log_constants::BLOCK_SIZE;
+const size_t data_log_constants::BUFFER_SIZE;
 
 typedef monolog::monolog_linear<uint8_t, data_log_constants::MAX_BLOCKS,
     data_log_constants::BLOCK_SIZE, data_log_constants::BUFFER_SIZE> data_log;
