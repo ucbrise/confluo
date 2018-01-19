8369f974b9a0db3a624bf6a2f60bf23e19f7d5f8
Ujval Misra <misraujval@gmail.com>
2018-01-19 11:32:13 -0800

    Use a log for transactions rather than a single header that's overwritten.

--- libconfluo/confluo/archival/io/incr_file_reader.h
+++ libconfluo/confluo/archival/io/incr_file_reader.h
@@ -3,21 +3,18 @@
 
 #include "exceptions.h"
 #include "incr_file_offset.h"
+#include "incr_file_stream.h"
 #include "file_utils.h"
 #include "io_utils.h"
 
 namespace confluo {
 namespace archival {
 
-class incremental_file_reader {
+class incremental_file_reader : public incremental_file_stream {
 
  public:
   incremental_file_reader(const std::string& path, const std::string& file_prefix)
-     : file_num_(0),
-       dir_path_(path),
-       file_prefix_(file_prefix) {
-    std::ifstream metadata_ifs(metadata_path());
-    last_file_num_ = io_utils::read<size_t>(metadata_ifs);
+      : incremental_file_stream(path, file_prefix) {
     open();
   }
 
@@ -54,32 +51,29 @@ class incremental_file_reader {
   }
 
   template<typename T>
-  T read_metadata() {
-    std::ifstream metadata_ifs(metadata_path());
-    metadata_ifs.seekg(METADATA_OFFSET);
-    return utils::io_utils::read<T>(metadata_ifs);
-  }
-
-  std::string cur_path() const {
-    return dir_path_ + "/" + file_prefix_ + "_" + std::to_string(file_num_) + ".dat";
-  }
-
-  std::string metadata_path() const {
-    return dir_path_ + "/" + file_prefix_ + "_metadata.dat";
+  T read_action() {
+    return utils::io_utils::read<T>(transaction_log_ifs_);
   }
 
   incremental_file_offset tell() {
-    // TODO handle EOF
     return incremental_file_offset(cur_path(), cur_ifs_.tellg());
   }
 
+  size_t tell_transaction_log() {
+    return transaction_log_ifs_.tellg();
+  }
+
   bool has_more() {
-    size_t off = cur_ifs_.tellg();
-    return off < eof_offset() || file_num_ < last_file_num_;
+    size_t off = transaction_log_ifs_.tellg();
+    transaction_log_ifs_.seekg(0, std::ios::end);
+    size_t end_off = transaction_log_ifs_.tellg();
+    transaction_log_ifs_.seekg(off);
+    return off < end_off;
   }
 
   void open() {
     cur_ifs_.open(cur_path());
+    transaction_log_ifs_.open(transaction_log_path());
   }
 
   bool is_open() {
@@ -115,13 +109,7 @@ class incremental_file_reader {
   }
 
   std::ifstream cur_ifs_;
-
-  size_t file_num_;
-  size_t last_file_num_;
-  std::string dir_path_;
-  std::string file_prefix_;
-
-  static const size_t METADATA_OFFSET = sizeof(size_t);
+  std::ifstream transaction_log_ifs_;
 
 };
 
--- libconfluo/confluo/archival/io/incr_file_writer.h
+++ libconfluo/confluo/archival/io/incr_file_writer.h
@@ -2,22 +2,22 @@
 #define CONFLUO_ARCHIVAL_INCR_FILE_WRITER_H_
 
 #include "incr_file_offset.h"
+#include "incr_file_stream.h"
 #include "file_utils.h"
 #include "io_utils.h"
 
 namespace confluo {
 namespace archival {
 
-class incremental_file_writer {
+class incremental_file_writer : public incremental_file_stream {
  public:
   incremental_file_writer(const std::string& path, const std::string& file_prefix, size_t max_file_size)
-      : file_num_(0),
-        dir_path_(path),
-        file_prefix_(file_prefix),
-        max_file_size_(max_file_size) {
+      : incremental_file_stream(path, file_prefix) {
+    max_file_size_ = max_file_size;
   }
 
-  incremental_file_writer(const incremental_file_writer& other) {
+  incremental_file_writer(const incremental_file_writer& other)
+      : incremental_file_stream() {
     close();
     file_num_ = other.file_num_;
     dir_path_ = other.dir_path_;
@@ -40,37 +40,37 @@ class incremental_file_writer {
     return *this;
   }
 
+  /**
+   * Initialize state.
+   */
   void init() {
-    if (file_utils::exists_file(metadata_path())) {
-      std::ifstream metadata_ifs(metadata_path());
-      file_num_ = io_utils::read<size_t>(metadata_ifs);
+    if (file_utils::exists_file(transaction_log_path())) {
+      std::ifstream transaction_log_ifs(transaction_log_path());
+      file_num_ = io_utils::read<size_t>(transaction_log_ifs);
       open();
     } else {
       cur_ofs_.open(cur_path());
-      metadata_ofs_.open(metadata_path());
-      update_last_file();
+      transaction_log_ofs_.open(transaction_log_path());
     }
   }
 
   template<typename T>
-  incremental_file_offset append(T* values, size_t len) {
+  incremental_file_offset append(T* data, size_t len) {
     if (!fits_in_cur_file(sizeof(T) * len))
       open_new_next();
     incremental_file_offset incr_file_off = tell();
-    io_utils::write<T>(cur_ofs_, values, len);
+    io_utils::write<T>(cur_ofs_, data, len);
     cur_ofs_.flush();
-    update_last_file();
     return incr_file_off;
   }
 
   template<typename T>
-  incremental_file_offset append(T value) {
+  incremental_file_offset append(T data) {
     if (!fits_in_cur_file(sizeof(T)))
       open_new_next();
     incremental_file_offset incr_file_off = tell();
-    io_utils::write<T>(cur_ofs_, value);
+    io_utils::write<T>(cur_ofs_, data);
     cur_ofs_.flush();
-    update_last_file();
     return incr_file_off;
   }
 
@@ -78,30 +78,20 @@ class incremental_file_writer {
    * TODO Need something more generic
    */
   template<typename T, typename U>
-  incremental_file_offset append(T* t_values, size_t t_len, U* u_values, size_t u_len) {
+  incremental_file_offset append(T* t_data, size_t t_len, U* u_data, size_t u_len) {
     if (!fits_in_cur_file((sizeof(T) * t_len) + (sizeof(U) * u_len)))
       open_new_next();
     incremental_file_offset incr_file_off = tell();
-    io_utils::write<T>(cur_ofs_, t_values, t_len);
-    io_utils::write<U>(cur_ofs_, u_values, u_len);
+    io_utils::write<T>(cur_ofs_, t_data, t_len);
+    io_utils::write<U>(cur_ofs_, u_data, u_len);
     cur_ofs_.flush();
-    update_last_file();
     return incr_file_off;
   }
 
-  template<typename T>
-  void update_metadata(T metadata) {
-    metadata_ofs_.seekp(METADATA_OFFSET);
-    io_utils::write<T>(metadata_ofs_, metadata);
-    metadata_ofs_.flush();
-  }
-
-  std::string cur_path() const {
-    return dir_path_ + "/" + file_prefix_ + "_" + std::to_string(file_num_) + ".dat";
-  }
-
-  std::string metadata_path() const {
-    return dir_path_ + "/" + file_prefix_ + "_metadata.dat";
+  template<typename ACTION>
+  void commit(ACTION action) {
+    io_utils::write<ACTION>(transaction_log_ofs_, action);
+    transaction_log_ofs_.flush();
   }
 
   incremental_file_offset tell() {
@@ -110,28 +100,23 @@ class incremental_file_writer {
 
   void flush() {
     cur_ofs_.flush();
-    metadata_ofs_.flush();
+    transaction_log_ofs_.flush();
   }
 
   void open() {
     // std::ios::in required to prevent truncation, caused by lack of std::ios::app
     cur_ofs_.open(cur_path(), std::ios::in | std::ios::out | std::ios::ate);
-    metadata_ofs_.open(metadata_path(), std::ios::in | std::ios::out | std::ios::ate);
+    transaction_log_ofs_.open(transaction_log_path(), std::ios::in | std::ios::out | std::ios::ate);
   }
 
   void close() {
     if (cur_ofs_.is_open())
       cur_ofs_.close();
-    if (metadata_ofs_.is_open())
-      metadata_ofs_.close();
+    if (transaction_log_ofs_.is_open())
+      transaction_log_ofs_.close();
   }
 
  private:
-  void update_last_file() {
-    metadata_ofs_.seekp(0);
-    io_utils::write<size_t>(metadata_ofs_, file_num_);
-    metadata_ofs_.flush();
-  }
 
   bool fits_in_cur_file(size_t append_size) {
     size_t off = cur_ofs_.tellp();
@@ -146,14 +131,9 @@ class incremental_file_writer {
   }
 
   std::ofstream cur_ofs_;
-  std::ofstream metadata_ofs_;
+  std::ofstream transaction_log_ofs_;
 
-  size_t file_num_;
-  std::string dir_path_;
-  std::string file_prefix_;
   size_t max_file_size_;
-
-  static const size_t METADATA_OFFSET = sizeof(size_t);
 
 };
 
