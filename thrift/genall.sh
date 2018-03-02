a626b6b0183829e8dcb1ddf108580d54744ee736
Anurag Khandelwal <anuragk@berkeley.edu>
2018-03-02 14:35:15 -0800

    Added thrift generator script

---
+++ thrift/genall.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+# Genrate cpp files
+thrift -r --gen cpp:templates,pure_enums,no_skeleton,include_prefix $sbin/rpc.thrift
+# Modify cpp extensions
+cpp_files=gen-cpp/*.cpp
+for file in $cpp_files; do
+    mv "$file" ${file/%.cpp/.cc}
+done
+
+# Move to destination
+mv gen-cpp/*.cc $sbin/../librpc/src/
+mv gen-cpp/* $sbin/../librpc/rpc/
+
+# Generate java files
+thrift -r --gen java:private-members,fullcamel,handle_runtime_exceptions,reuse-objects $sbin/rpc.thrift
+
+# Move to destination
+rsync -r --remove-source-files  gen-java/confluo $sbin/../javaclient/src/main/java
+
+# Generate python files
+thrift -r --gen py $sbin/rpc.thrift
+
+# Move to destination
+rsync -r --remove-source-files gen-py/confluo $sbin/../pyclient/
