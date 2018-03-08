a5128b3f458a129e54e685ed70ff94f896f3d5dd
ming li <mingyli34@gmail.com>
2018-03-07 22:06:45 -0800

    Add json methods to thrift

--- thrift/rpc.thrift
+++ thrift/rpc.thrift
@@ -97,6 +97,10 @@ service rpc_service {
   rpc_atomic_multilog_info get_atomic_multilog_info(1: string name),
   void remove_atomic_multilog(1: i64 multilog_id)
           throws (1: rpc_management_exception ex),
+
+  // Execute function specified by JSON
+  string run_command(1: i64 multilog_id, 2: string json_command)
+          throws (1: rpc_management_exception ex),
   
   // Table ops
   void add_index(1: i64 multilog_id, 2: string field_name, 
@@ -126,6 +130,7 @@ service rpc_service {
   
   // Read ops
   binary read(1: i64 multilog_id, 2: i64 offset, 3: i64 nrecords),
+  string read_json(1: i64 multilog_id, 2: i64 offset),
   string query_aggregate(1: i64 multilog_id, 2: string aggregate_name, 
                          3: i64 begin_ms, 4: i64 end_ms)
           throws (1: rpc_invalid_operation ex),
