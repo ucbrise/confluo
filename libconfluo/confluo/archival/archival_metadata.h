a92814e9b010a3a9445050aa705ac75f312c23ec
Ujval Misra <misraujval@gmail.com>
2018-03-11 13:44:31 -0700

    More documentation.

--- libconfluo/confluo/archival/archival_metadata.h
+++ libconfluo/confluo/archival/archival_metadata.h
@@ -9,16 +9,27 @@ namespace confluo {
 namespace archival {
 
 // TODO distinction between metadata & transaction log actions:
-// most recent commit should describe everything before it,
-// everything else should go here. Need to separate it out better.
-// That way old actions can be garbage collected.
+// most recent commit should describe everything before it, and
+// make recovery possible. Everything else should go here.
+// Need to separate it out better.
 
 class filter_aggregates_archival_metadata {
  public:
+  /**
+   * Constructor.
+   * @param ts_block timestamp block corresponding to reflog containing aggregates
+   * @param version aggregates version
+   * @param num_aggs number of aggregates
+   */
   filter_aggregates_archival_metadata(byte_string ts_block, size_t version, size_t num_aggs)
       : ts_block_(ts_block), version_(version), num_aggs_(num_aggs) {
   }
 
+  /**
+   * Read metadata from archives reader.
+   * @param reader archives reader
+   * @return metadata
+   */
   static filter_aggregates_archival_metadata read(incremental_file_reader& reader) {
     byte_string ts_block = byte_string(reader.read(sizeof(uint64_t)));
     size_t version = reader.read<size_t>();
@@ -26,20 +37,37 @@ class filter_aggregates_archival_metadata {
     return filter_aggregates_archival_metadata(ts_block, version, num_aggs);
   }
 
+  /**
+   * Append metadata to file.
+   * @param metadata archival metadata
+   * @param writer archives writer
+   */
   static void append(filter_aggregates_archival_metadata metadata, incremental_file_writer& writer) {
     writer.append<uint8_t>(metadata.ts_block_.data(), sizeof(uint64_t));
     writer.append<size_t>(metadata.version_);
     writer.append<size_t>(metadata.num_aggs_);
   }
 
+  /**
+   *
+   * @return timestamp block corresponding to reflog containing aggregates
+   */
   byte_string ts_block() {
     return ts_block_;
   }
 
+  /**
+   *
+   * @return aggregates version
+   */
   size_t version() {
     return version_;
   }
 
+  /**
+   *
+   * @return number of aggregates
+   */
   size_t num_aggregates() {
     return num_aggs_;
   }
@@ -52,10 +80,22 @@ class filter_aggregates_archival_metadata {
 
 class radix_tree_archival_metadata {
  public:
+  /**
+   * Constructor.
+   * @param key radix tree key
+   * @param reflog_idx reflog index of archived bucket
+   * @param bucket_size bucket size
+   */
   radix_tree_archival_metadata(byte_string key, size_t reflog_idx, size_t bucket_size)
       : key_(key), reflog_index_(reflog_idx), bucket_size_(bucket_size) {
   }
 
+  /**
+   * Read metadata from archives reader.
+   * @param reader archives reader
+   * @param key_size size of radix tree key
+   * @return metadata
+   */
   static radix_tree_archival_metadata read(incremental_file_reader& reader, size_t key_size) {
     byte_string key = byte_string(reader.read(key_size));
     size_t reflog_idx = reader.read<size_t>();
@@ -63,20 +103,37 @@ class radix_tree_archival_metadata {
     return radix_tree_archival_metadata(key, reflog_idx, bucket_len);
   }
 
+  /**
+   * Append metadata to file.
+   * @param metadata archival metadata
+   * @param writer archives writer
+   */
   static void append(radix_tree_archival_metadata metadata, incremental_file_writer& writer) {
     writer.append<uint8_t>(metadata.key_.data(), metadata.key_.size());
     writer.append<size_t>(metadata.reflog_index_);
     writer.append<size_t>(metadata.bucket_size_);
   }
 
+  /**
+   *
+   * @return corresponding radix tree key of the reflog archived
+   */
   byte_string key() {
     return key_;
   }
 
+  /**
+   *
+   * @return reflog index of archived bucket
+   */
   size_t reflog_index() {
     return reflog_index_;
   }
 
+  /**
+   *
+   * @return size of archived bucket
+   */
   size_t bucket_size() {
     return bucket_size_;
   }
