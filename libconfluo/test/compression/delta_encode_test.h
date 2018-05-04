d72263f4b6a27e6abb7ba639a85dff02a4508393
Ujval Misra <misraujval@gmail.com>
2018-05-03 19:35:31 -0700

    Modified tests to reflect iface changes.

--- libconfluo/test/compression/delta_encode_test.h
+++ libconfluo/test/compression/delta_encode_test.h
@@ -23,16 +23,14 @@ TEST_F(DeltaEncodeTest, DecodeFullTest) {
   }
 
   uint64_t* dest_buffer = new uint64_t[k_array_size];
-
-  uint8_t* encode_buffer = delta_encoder::encode(array, k_array_size);
-  delta_decoder::decode(encode_buffer, dest_buffer);
+  auto encoded_buffer = delta_encoder::encode(array, k_array_size);
+  delta_decoder::decode<uint64_t>(encoded_buffer.get(), dest_buffer);
 
   for (size_t i = 0; i < k_array_size; i++) {
     ASSERT_EQ(array[i], dest_buffer[i]);
   }
 
   delete[] array;
-  delete[] encode_buffer;
   delete[] dest_buffer;
 }
 
@@ -49,15 +47,19 @@ TEST_F(DeltaEncodeTest, DecodePartialTest) {
 
   uint64_t* dest_buffer = new uint64_t[buffer_size];
 
+<<<<<<< HEAD
   uint8_t* encode_buffer = delta_encoder::encode(array, k_array_size);
   delta_decoder::decode(encode_buffer, dest_buffer, src_index, buffer_size);
+=======
+  auto encoded_buffer = delta_encoder::encode(array, k_array_size);
+  delta_decoder::decode(encoded_buffer.get(), dest_buffer, src_index, buffer_size);
+>>>>>>> Modified tests to reflect iface changes.
 
   for (size_t i = 0; i < buffer_size; i++) {
     ASSERT_EQ(array[i + src_index], dest_buffer[i]);
   }
 
   delete[] array;
-  delete[] encode_buffer;
   delete[] dest_buffer;
 }
 
@@ -72,18 +74,24 @@ TEST_F(DeltaEncodeTest, DecodePtrIndexTest) {
   size_t src_index = 250;
   size_t buffer_size = k_array_size - src_index;
 
+<<<<<<< HEAD
 
   uint64_t* dest_buffer = new uint64_t[buffer_size];
   uint8_t* encode_buffer = delta_encoder::encode(array, k_array_size);
 
   delta_decoder::decode(encode_buffer, src_index, dest_buffer);
+=======
+  uint64_t* dest_buffer = new uint64_t[buffer_size];
+  auto encoded_buffer = delta_encoder::encode(array, k_array_size);
+
+  delta_decoder::decode(encoded_buffer.get(), dest_buffer, src_index);
+>>>>>>> Modified tests to reflect iface changes.
 
   for (size_t i = 0; i < buffer_size; i++) {
     ASSERT_EQ(array[i + src_index], dest_buffer[i]);
   }
 
   delete[] array;
-  delete[] encode_buffer;
   delete[] dest_buffer;
 }
 
@@ -96,13 +104,17 @@ TEST_F(DeltaEncodeTest, DecodeIndexTest) {
   }
 
   size_t src_index = 250;
+<<<<<<< HEAD
   uint8_t* encode_buffer = delta_encoder::encode(array, k_array_size);
   uint8_t decoded_val = delta_decoder::decode(encode_buffer, src_index);
+=======
+  auto encoded_buffer = delta_encoder::encode(array, k_array_size);
+  uint8_t decoded_val = delta_decoder::decode<uint64_t>(encoded_buffer.get(), src_index);
+>>>>>>> Modified tests to reflect iface changes.
 
   ASSERT_EQ(array[src_index], decoded_val);
 
   delete[] array;
-  delete[] encode_buffer;
 }
 
 
