0761a52334cd1bfb7b660c093e40821644c02464
Anurag Khandelwal <anuragk@berkeley.edu>
2018-06-04 16:13:37 -0700

    Fix alert comparator definition

--- libconfluo/src/alert.cc
+++ libconfluo/src/alert.cc
@@ -1,17 +1,18 @@
 #include "alert.h"
 
 namespace confluo {
+namespace monitor {
 
-monitor::alert::alert()
+alert::alert()
     : version(0),
       time_bucket(0) {
 }
 
-monitor::alert::alert(uint64_t _time_bucket,
-                      const std::string &trigger_name,
-                      const std::string &trigger_expr,
-                      const numeric &_value,
-                      uint64_t _version)
+alert::alert(uint64_t _time_bucket,
+             const std::string &trigger_name,
+             const std::string &trigger_expr,
+             const numeric &_value,
+             uint64_t _version)
     : trigger_name(trigger_name),
       trigger_expr(trigger_expr),
       value(_value),
@@ -19,14 +20,13 @@ monitor::alert::alert(uint64_t _time_bucket,
       time_bucket(_time_bucket) {
 }
 
-std::string monitor::alert::to_string() const {
-  return "{ timestamp: " + std::to_string(time_bucket) + ", trigger-name: \""
-      + trigger_name + "\", trigger-expression: \"" + trigger_expr
-      + "\", trigger-value: \"" + value.to_string() + "\", version: "
+std::string alert::to_string() const {
+  return "{ timestamp: " + std::to_string(time_bucket) + ", trigger-name: \"" + trigger_name
+      + "\", trigger-expression: \"" + trigger_expr + "\", trigger-value: \"" + value.to_string() + "\", version: "
       + std::to_string(version) + " }";
 }
 
-bool monitor::operator<(const monitor::alert &left, const monitor::alert &right) {
+bool operator<(const alert &left, const alert &right) {
   if (left.time_bucket == right.time_bucket
       && left.trigger_name == right.trigger_name) {
     return left.value < right.value;
@@ -36,4 +36,5 @@ bool monitor::operator<(const monitor::alert &left, const monitor::alert &right)
   return left.time_bucket < right.time_bucket;
 }
 
+}
 }
