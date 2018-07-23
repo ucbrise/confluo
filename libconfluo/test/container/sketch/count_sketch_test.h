9dac9b3177606394756501213e23c648e5170d7a
Ujval Misra <misraujval@gmail.com>
2018-07-23 04:40:25 -0700

    Test with zipf, change error measurements.
    * Measure errors for elements with frequencies exceeding alpha threshold (count sketch). This shows a much more reasonable trend in the errors (parameters respected). Doesn't make sense to measure median over all errors since this includes infrequent elements.
    
    * Zipf handled well by universal sketch.
    
    * Need to rewrite tests / remove heavy computation from them and put somewhere else (only test correctness).

--- libconfluo/test/container/sketch/count_sketch_test.h
+++ libconfluo/test/container/sketch/count_sketch_test.h
@@ -3,6 +3,7 @@
 
 #include <functional>
 
+#include "/Users/Ujval/dev/research/univmon_extension/simulator/V0.3/countSketch.h"
 #include "container/sketch/count_sketch.h"
 #include "gtest/gtest.h"
 
@@ -10,46 +11,106 @@ using namespace ::confluo::sketch;
 
 class CountSketchTest : public testing::Test {
  public:
-  static const int N = 10000;
+  static constexpr int N = 1000000;
+
+  static constexpr double MEAN = 0.0;
+  static constexpr double SD = 500.0;
+
+  static constexpr int X0 = 1;
+  static constexpr int X1 = 1000000;
+
+  CountSketchTest()
+      : dist(MEAN, SD),
+        udist(0.0, 1.0) {
+    std::random_device rd;
+    e2 = std::mt19937(rd());
+  }
+
+  int sample_zipf(double alpha = 1.5) {
+    static bool first = true;      // Static first time flag
+    static double c = 0;          // Normalization constant
+    static double* sum_probs;     // Pre-calculated sum of probabilities
+    double z = 0.0;               // Uniform random number (0 < z < 1)
+    int zipf_value = 0;           // Computed exponential value to be returned
+    int i;                        // Loop counter
+    int low, high, mid;           // Binary-search bounds
+
+    // Compute normalization constant on first call only
+    if (first) {
+      for (i = X0; i <= X1; i++)
+        c = c + (1.0 / pow((double) i, alpha));
+      c = 1.0 / c;
+      sum_probs = new double[X1+1];
+      sum_probs[0] = 0;
+      for (i = X0; i <= X1; i++)
+        sum_probs[i] = sum_probs[i-1] + c / pow((double) i, alpha);
+      first = false;
+    }
+
+    while ((z == 0) || (z == 1)) // (0 < z < 1)
+      z = udist(e2);
+
+    low = X0, high = X1, mid = 0;
+    while (low <= high) {
+      mid = std::floor((low+high)/2);
+      if (sum_probs[mid] >= z && sum_probs[mid-1] < z) {
+        zipf_value = mid;
+        break;
+      }
+      else if (sum_probs[mid] >= z) {
+        high = mid-1;
+      }
+      else {
+        low = mid+1;
+      }
+    }
+    assert((zipf_value >= X0) && (zipf_value <= X1));
+    return(zipf_value);
+  }
+
+  double generate_zipf(std::map<int, long int>& hist) {
+    for (int n = 0; n < N; n++)
+      hist[std::abs(sample_zipf(1.2))]++;
+
+    double l2_sq = 0.0;
+    for (auto p : hist)
+      l2_sq += (p.second * p.second);
+
+    return std::sqrt(l2_sq);
+  }
+
+ private:
+  std::normal_distribution<double> dist;
+  std::uniform_real_distribution<> udist;
+  std::mt19937 e2;
+
 };
 
 const int CountSketchTest::N;
 
 TEST_F(CountSketchTest, EstimateAccuracyTest) {
 
-  double epsilon = 0.0;
+  double alpha = 0.01;
+  double epsilon[] = { 0.01, 0.02, 0.04, 0.08, 0.16 };
+  double gamma[] = { 0.01 };
 
-  std::ofstream out("sketch_error.out");
-
-  std::random_device rd;
-  std::mt19937 e2(rd());
-  std::normal_distribution<double> dist(0, 100);
   std::map<int, long int> hist;
+  double l2 = generate_zipf(hist);
+  std::ofstream out("sketch_error.out");
+  LOG_INFO << "L2-norm: " << l2;
 
-  for (int n = 0; n < N; n++) {
-    hist[std::abs(std::round(dist(e2)))]++;
-  }
+  for (double g : gamma) {
+    for (double e : epsilon) {
 
-  size_t num_rows[] = { 4, 8, 16, 32 };
-  size_t counters_per_row[] = {  8000 };
+      auto cs = count_sketch<int>::create_parameterized(g, e);
 
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 1; j++) {
-
-      count_sketch<int> cs(num_rows[i], counters_per_row[j]);
-//      CountSketch alan_cs(counters_per_row[j], num_rows[i], 10000);
-
-      for (auto p : hist) {
-        for (int i = 0; i < p.second; i++) {
+      for (auto p : hist)
+        for (int i = 0; i < p.second; i++)
           cs.update(p.first);
-//          alan_cs.add(p.first);
-        }
-      }
 
       double num_measurements = 0;
-      double violator_count = 0;
       std::vector<double> errors;
-//      std::vector<double> alan_errors;
+      std::vector<double> relevant_errors;
 
       size_t negative_count = 0;
       for (auto p : hist) {
@@ -62,40 +123,36 @@ TEST_F(CountSketchTest, EstimateAccuracyTest) {
         double error = diff/actual;
         errors.push_back(error);
 
-        //if (error > epsilon) {
-        //  violator_count++;
-        //}
+        if (actual > alpha * l2) {
+          relevant_errors.push_back(error);
+        }
+
         if (est < 0) {
           negative_count++;
         }
-
-        // Alan estimate
-//        int alan_est = alan_cs.estimate(p.first);
-//        double alan_diff = std::abs(alan_est - actual);
-//        double alan_error = alan_diff/actual;
-//        alan_errors.push_back(alan_error);
-
       }
 
       // Compute overall stats
       std::sort(errors.begin(), errors.end());
-      double average_error = std::accumulate(errors.begin(), errors.end(), 0.0)/errors.size();
+      double avg_err = std::accumulate(errors.begin(), errors.end(), 0.0)/errors.size();
 
-//      std::sort(alan_errors.begin(), alan_errors.end());
-//      double alan_average = std::accumulate(alan_errors.begin(), alan_errors.end(), 0.0)/alan_errors.size();
+      std::sort(relevant_errors.begin(), relevant_errors.end());
+      double avg_relv_err = std::accumulate(relevant_errors.begin(), relevant_errors.end(), 0.0)/relevant_errors.size();
 
       // Print
-      LOG_INFO << "Dimensions: " << num_rows[i] << " x " << counters_per_row[j];
+      LOG_INFO << "Dimensions: " << cs.depth() << " x " << cs.width();
       LOG_INFO << "Sketch size: " << (cs.storage_size() / 1024) << " KB";
       LOG_INFO << "Negative rate: " << negative_count/num_measurements;
       LOG_INFO << "Median error: " << errors[int(errors.size()/2)];
-      LOG_INFO << "Mean error: " << average_error << "\n";
-     // LOG_INFO << "Median error (alan): " << alan_errors[int(alan_errors.size()/2)] << "\n";
+      LOG_INFO << "Median relevant error: " << relevant_errors[int(relevant_errors.size()/2)];
+      LOG_INFO << "Mean error: " << avg_err;
+      LOG_INFO << "Mean relevant error: " << avg_relv_err;
+      LOG_INFO << "";
 
-      //LOG_INFO<< "Incorrectness rate: " << violator_count/num_measurements;
-      //std::cout << "Failure rate: " << violator_count/num_measurements << "\n";
-      //summary_out << num_rows[i] << " " << counters_per_row[j] << " " << violator_count/num_measurements <<
-      // " " << errors[int(errors.size()/2)] << "\n";
+      ASSERT_LT(avg_relv_err, e);
+
+//      summary_out << num_rows[i] << " " << counters_per_row[j] << " " << violator_count/num_measurements <<
+//       " " << errors[int(errors.size()/2)] << "\n";
 
     }
   }
