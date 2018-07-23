3d46249714d4d67dc45b622bb381175d1e5bb655
Ujval Misra <misraujval@gmail.com>
2018-07-23 04:40:25 -0700

    Test with zipf, change error measurements.
    * Measure errors for elements with frequencies exceeding alpha threshold (count sketch). This shows a much more reasonable trend in the errors (parameters respected). Doesn't make sense to measure median over all errors since this includes infrequent elements.
    
    * Zipf handled well by universal sketch.
    
    * Need to rewrite tests / remove heavy computation from them and put somewhere else (only test correctness).

--- libconfluo/test/container/sketch/universal_sketch_test.h
+++ libconfluo/test/container/sketch/universal_sketch_test.h
@@ -8,9 +8,72 @@ using namespace ::confluo::sketch;
 
 class UniversalSketchTest : public testing::Test {
 public:
-  static const int N = 10000;
-  static const int mean = 0;
-  static const int variance = 100;
+  static constexpr int N = 1000000;
+  static constexpr int MEAN = 0;
+  static constexpr int SD = 400;
+
+  static constexpr int X0 = 1;
+  static constexpr int X1 = 10000000;
+
+  UniversalSketchTest()
+          : dist(MEAN, SD),
+            udist(0.0, 1.0) {
+    std::random_device rd;
+    e2 = std::mt19937(rd());
+  }
+
+  int sample_zipf(double alpha = 1.5) {
+    static bool first = true;      // Static first time flag
+    static double c = 0;          // Normalization constant
+    static double* sum_probs;     // Pre-calculated sum of probabilities
+    double z = 0.0;               // Uniform random number (0 < z < 1)
+    int zipf_value = 0;           // Computed exponential value to be returned
+    int i;                        // Loop counter
+    int low, high, mid;           // Binary-search bounds
+
+    // Compute normalization constant on first call only
+    if (first) {
+      for (i = X0; i <= X1; i++)
+        c = c + (1.0 / pow((double) i, alpha));
+      c = 1.0 / c;
+      sum_probs = new double[X1+1];
+      sum_probs[0] = 0;
+      for (i = X0; i <= X1; i++)
+        sum_probs[i] = sum_probs[i-1] + c / pow((double) i, alpha);
+      first = false;
+    }
+
+    while ((z == 0) || (z == 1)) // (0 < z < 1)
+      z = udist(e2);
+
+    low = X0, high = X1, mid = 0;
+    while (low <= high) {
+      mid = std::floor((low+high)/2);
+      if (sum_probs[mid] >= z && sum_probs[mid-1] < z) {
+        zipf_value = mid;
+        break;
+      }
+      else if (sum_probs[mid] >= z) {
+        high = mid-1;
+      }
+      else {
+        low = mid+1;
+      }
+    }
+    assert((zipf_value >= X0) && (zipf_value <= X1));
+    return(zipf_value);
+  }
+
+  double generate_zipf(std::map<int, long int>& hist) {
+    for (int n = 0; n < N; n++)
+      hist[std::abs(sample_zipf(1.2))]++;
+
+    double l2_sq = 0.0;
+    for (auto p : hist)
+      l2_sq += (p.second * p.second);
+
+    return std::sqrt(l2_sq);
+  }
 
   static int64_t f0(int64_t x) {
     return 1;
@@ -24,10 +87,9 @@ public:
     return x * x;
   }
 
-  static void benchmark(size_t l, size_t t, size_t b, size_t k, double a, std::map<int, long int> hist) {
+  static void eval(universal_sketch<int>& us, std::map<int, long int>& hist, size_t layers) {
 
-    universal_sketch<int> us(l, t, b, k, a);
-    std::function<int64_t(int64_t)> g = f1;
+    std::function<int64_t(int64_t)> g = f2;
 
     int64_t actual = 0;
 
@@ -40,22 +102,27 @@ public:
     }
     int64_t update_b = time_utils::cur_ns();
 
-    int64_t eval_a = time_utils::cur_ns();
-    int64_t g_sum = us.evaluate(g);
-    int64_t eval_b = time_utils::cur_ns();
+    for (size_t l = 1; l < layers; l++) {
+      int64_t eval_a = time_utils::cur_ns();
+      int64_t g_sum = us.evaluate(g, l);
+      int64_t eval_b = time_utils::cur_ns();
 
-    for (auto p : hist) {
-      ASSERT_EQ(us.estimate_count(p.first), p.second);
+      LOG_INFO << "Layers: " << l;
+      LOG_INFO << g_sum << " vs " << actual;
+      LOG_INFO << "Storage size: " << us.storage_size()/1024 << " KB";
+      LOG_INFO << "Error: " << double(std::abs(g_sum - actual))/actual;
+      //LOG_INFO << "Latency per update: " << (update_b - update_a)/1000 << " us";
+      LOG_INFO << "Evaluation latency: " << (eval_b - eval_a)/1000 << " us";
+      LOG_INFO << "";
     }
 
-    LOG_INFO << g_sum << " vs " << actual;
-    LOG_INFO << "Storage size: " << us.storage_size()/1024 << " KB";
-    LOG_INFO << "Error: " << double(std::abs(g_sum - actual))/actual;
-    LOG_INFO << "Latency per update: " << (update_b - update_a)/1000 << " us";
-    LOG_INFO << "Evaluation latency: " << (eval_b - eval_a)/1000 << " us";
-    LOG_INFO << "";
-
   }
+
+ private:
+  std::normal_distribution<double> dist;
+  std::uniform_real_distribution<> udist;
+  std::mt19937 e2;
+
 };
 
 TEST_F(UniversalSketchTest, EstimateAccuracyTest) {
@@ -64,21 +131,16 @@ TEST_F(UniversalSketchTest, EstimateAccuracyTest) {
 
   std::random_device rd;
   std::mt19937 e2(rd());
-  std::normal_distribution<double> dist(mean, variance);
   std::map<int, long int> hist;
+  double l2 = generate_zipf(hist);
 
-  for (int n = 0; n < N; n++) {
-    hist[std::abs(std::round(dist(e2)))]++;
-  }
-
-  size_t l = 5;
-  size_t t = 32;
+  size_t l = 12;
+  size_t t = 64;
   size_t b = 27000;
-  size_t k = 100;
+  size_t k = 20;
   double a = 0;
-
-  for (size_t layers = 1; layers < l; layers++)
-    benchmark(layers, t, b, k, a, hist);
+  universal_sketch<int> us(l, t, b, k, a);
+  eval(us, hist, l);
 
 }
 
