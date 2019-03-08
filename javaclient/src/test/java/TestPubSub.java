ae737362d59902f9d8d462921941a7ca3484163b
Rudy Steiner <rudy_steiner@163.com>
2019-03-08 17:43:34 +0800

    remove useless test

--- javaclient/src/test/java/TestPubSub.java
+++ javaclient/src/test/java/TestPubSub.java
@@ -25,21 +25,7 @@ public class TestPubSub {
       'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'e', 'e', 'f', 'g', 'h', 'i', 'j',
       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     };
-
-
-  @Test
-  public void produce() {
-    ProduceTask produceTask = new ProduceTask(10000, 5000);
-    produceTask.run();
-  }
-
-  @Test
-  public void consume() {
-    ConsumeTask consumeTask = new ConsumeTask(10000, 10000, 0);
-    consumeTask.run();
-  }
-
-
+  
   @Test
   public void testConfluoPubSub()throws IOException, TException {
     Properties producerProperties = PropertiesParser.parse("mq.produce", "mq.properties");
